56675f12627ee5dfe104918c0778c5148b6f87aa
Moses Hunsu <hunsumoses7@gmail.com>
2023-07-04 12:38:21 +0100

    Initial commit

---
+++ src/main/java/com/sunpower/SunPowerApplication.java
@@ -0,0 +1,13 @@
+package com.sunpower;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunPowerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SunPowerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sunpower/SunPowerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunpower;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunPowerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
